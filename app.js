4e73efb689aaf2895edd1e2acba8ae969c3b6cb3
berkanrhdz <alu0100815733@ull.edu.es>
2018-03-18 16:17:46 +0000

    Fixes some problems with Mac Support.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 'use strict'
 
 const electron = require('electron')
-const window = require('./config/electron/window')
+const mainwindow = require('./config/electron/window')
 const app = electron.app // Module to control application life.
 
 /* This method will be called when Electron has finished 
 initialization and is ready to create browser windows. 
 Call the function imported from window.js*/
-app.on('ready', window.createWindow)
+app.on('ready', mainwindow.createWindow)
 
 /* Quit when all windows are closed. */
 app.on('window-all-closed', function () {
@@ -18,11 +18,7 @@ app.on('window-all-closed', function () {
   }
 })
 
-app.on('activate', function () {
-  /* On OS X it's common to re-create a window in the app when the
-  dock icon is clicked and there are no other windows open.*/
-  if (window === null) {
-    createWindow()
-  }
-})
+
+
+
 
--- config/electron/window.js
+++ config/electron/window.js
@@ -6,36 +6,42 @@ const url = require('url')
 const app = electron.app // Module to control application life.
 const BrowserWindow = electron.BrowserWindow // Module to create native browser window.
 
-
-let win /// Necessary Keep a global reference of the window object.
+let win // Necessary Keep a global reference of the window object.
 
 /* Function to create the mainwindow of the electron app
-The function is exported to use when electron is inicializate in app.js*/
-module.exports = {
-  createWindow : function () {
-    win = new BrowserWindow({ // Create the window.
-      title: "App Title",
+The function is exported to use when electron is inicializate in app.js. */
+function createWindow() {
+  win = new BrowserWindow({ // Create the window.
+    title: "App Title",
+    width: 1281,
+    height: 800, 
+    minWidth: 1281,
+    minHeight: 800,
+    backgroundColor: '#312450'
+  //  icon: path.join(__dirname, 'assets/icons/png/64x64.png')
+  })
+  // Load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
-      width: 1281,
-      height: 800, 
-      minWidth: 1281,
-      minHeight: 800,
-      backgroundColor: '#234150'
-    //  icon: path.join(__dirname, 'assets/icons/png/64x64.png')
-    })
-    // Load the index.html of the app.
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }))
-
-    // Emitted when the window is closed.
-    win.on('closed', function () {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      win = null
-    })
-  }
+  // Emitted when the window is closed.
+  win.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
 }
+  
+app.on('activate', function () {
+  /* On OS X it's common to re-create a window in the app when the
+  dock icon is clicked and there are no other windows open. */
+  if (win === null) {
+    createWindow()
+  }
+})
+  
+module.exports.createWindow = createWindow
