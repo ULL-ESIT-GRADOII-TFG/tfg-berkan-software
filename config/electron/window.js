830ba202193577f5c5e97c0048b2676c6412240d
berkanrhdz <alu0100815733@ull.edu.es>
2018-03-18 14:40:49 +0000

    Configures Electron mainwindow.

---
+++ config/electron/window.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const electron = require('electron') 
+const path = require('path')
+const url = require('url')
+const app = electron.app // Module to control application life.
+const BrowserWindow = electron.BrowserWindow // Module to create native browser window.
+
+
+let win /// Necessary Keep a global reference of the window object.
+
+/* Function to create the mainwindow of the electron app
+The function is exported to use when electron is inicializate in app.js*/
+module.exports = {
+  createWindow : function () {
+    win = new BrowserWindow({ // Create the window.
+      title: "App Title",
+
+      width: 1281,
+      height: 800, 
+      minWidth: 1281,
+      minHeight: 800,
+      backgroundColor: '#234150'
+    //  icon: path.join(__dirname, 'assets/icons/png/64x64.png')
+    })
+    // Load the index.html of the app.
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+
+    // Emitted when the window is closed.
+    win.on('closed', function () {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      win = null
+    })
+  }
+}
