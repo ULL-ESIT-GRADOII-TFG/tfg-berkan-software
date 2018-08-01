de563176248765fb6461fafb8a7f3c7c4cea7818
berkanrhdz <alu0100815733@ull.edu.es>
2018-08-01 23:36:40 +0100

    Adds messages alert for dont enter an empty assignment.

--- resources/assets/js/renderers/new_assignment.js
+++ resources/assets/js/renderers/new_assignment.js
@@ -2,5 +2,20 @@ function newAssignment (orgName, orgAvatar) {
   var assignmentName = document.getElementById('assignment-name').value
   var assignmentRegex = document.getElementById('assignment-regex').value
   
-  ipcRender.send('render-newassignment', [orgName, assignmentName, assignmentRegex, orgAvatar])
+  if (assignmentName == '' && assignmentRegex == '') {
+    alert('You must enter assignment regex and name values')
+    document.getElementById('assignment-regex').style.border = "1px solid red";
+    document.getElementById('assignment-name').style.border = "1px solid red";
+  } else if (assignmentRegex == '') {
+    alert('You must enter assignment regex value')
+    document.getElementById('assignment-regex').style.border = "1px solid red";
+    document.getElementById('assignment-name').style.border = "1px solid lightgray";
+  } else if (assignmentName == '') {
+    alert('You must enter assignment name value')
+    document.getElementById('assignment-name').style.border = "1px solid red";  
+    document.getElementById('assignment-regex').style.border = "1px solid lightgray";
+  } else {
+    ipcRender.send('render-newassignment', [orgName, assignmentName, assignmentRegex, orgAvatar])
+  }
+
 }
